39cbd78823df1caea29d7680f0b7e16909941c47
Alpha <121655660+KARTHIK-004@users.noreply.github.com>
2024-01-31 20:30:58 +0530

    connected db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express()
 
 app.listen(3000, ()=>{
-    console.log("Sever is running !!!")
+    console.log("server is running at 3000!!")
 })
